5e20df836c7a156e5bfaa7215949a7089c64e513
njedhir <58675734+njedhir@users.noreply.github.com>
2022-01-18 19:01:10 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const moment = require('moment')
+const util = require('util')
+module.exports = (msgs, isError = false) => {
+  const writer = isError ? console.error : console.log
+  writer(moment.utc().format(), ':')
+  msgs.forEach(msg => {
+    writer(util.inspect(msg, false, null, true))
+  })
+  writer('----\n')
+}
