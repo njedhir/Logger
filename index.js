b6a6b9d43e7dc97599a880628d52493ecc7cd87b
Wahyu Widodo <58675734+njedhir@users.noreply.github.com>
2022-08-31 11:27:29 +0700

    Remove util

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const moment = require('moment')
-const util = require('util')
+// const util = require('util')
 module.exports = (msgs, isError = false) => {
   const writer = isError ? console.error : console.log
   writer(moment.utc().format(), ':')
   msgs.forEach(msg => {
-    writer(util.inspect(msg, false, null, true))
+    // writer(util.inspect(msg, false, null, true))
+    writer(msg)
   })
   writer('----\n')
 }
