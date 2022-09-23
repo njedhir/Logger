2dcd6aaf71ba47877a403e90a0a21c8eb856a9c6
Wahyu Widodo <58675734+njedhir@users.noreply.github.com>
2022-09-23 18:30:19 +0700

    Use cli-color and add setVerbose, setDelimiter, write log. Add moduleName, functionName, eventName

--- README.md
+++ README.md
@@ -3,28 +3,34 @@
  
  ## How to use
  ```javascript
- const logger = require('logger')
- 
+ const logger = require('njedhir-logger')
+
+const moduleName = 'TCPServer'
+const functionName = 'onSocketError'
+const eventName = 'Socket Error'
 const msg = 'Your log message here'
 const msg2 = 'Another log message'
 
+// You can set logger verbosity (Default: true)
+logger.setVerbose(false) // Logger won't output anything
+
+// You can set logger delimiter
+// Logger won't put delimiter after log message
+logger.setDelimiter(false)
+
+// Logger will put delimiter with string of the second param
+// If you omit second param, the default value is '____\n'
+logger.setDelimiter(true, '----')
+
 // log msg with logger
-logger([msg])
-
-// you can add more message
-logger([msg, msg2])
-
-// to log an error use:
-logger([msg, msg2], true)
+// if isError is true, then logger will put red color on the time stamp
+logger.write(moduleName, functionName, eventName, isError, msg1, msg2)
 ```
 
 The result of above code will be like the following:
 ```log
-2022-01-19T06:07:08Z :
-Your log message here
-----
-
-2022-01-19T06:07:09Z :
+2022-09-23T11:26:39Z
+TCP Server > onSocketError > Socket Error
 Your log message here
 Another log message
 ----
--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const moment = require('moment')
-// const util = require('util')
-module.exports = (msgs, isError = false) => {
-  const writer = isError ? console.error : console.log
-  writer(moment.utc().format(), ':')
+const clc = require('cli-color')
+const log = console.log
+let verbose = true
+let delimiter = true
+let delimiterChar = '____\n'
+const timeColor = clc.green.underline
+const timeColorError = clc.red.underline
+const moduleColor = clc.blue.bold
+const functionColor = clc.blue.italic
+const eventColor = clc.yellow.italic
+
+const setVerbose = isVerbose => {
+  verbose = isVerbose
+}
+
+const setDelimiter = (isDelimiter, delimiterString = '____\n') => {
+  delimiter = isDelimiter
+  delimiterChar = delimiterString
+}
+
+const write = (moduleName, functionName, eventName, isError = false, ...msgs) => {
+  if (!verbose) return
+  const curTime = moment.utc().format()
+  isError ? log(timeColorError(curTime)) : log(timeColor(curTime))
+  let modLogs = moduleName ? moduleColor(moduleName) : ''
+  modLogs += functionName ? ' > ' + functionColor(functionName) : ''
+  modLogs += eventName ? ' > ' + eventColor(eventName) : ''
+  if (moduleName) log(modLogs)
   msgs.forEach(msg => {
-    // writer(util.inspect(msg, false, null, true))
-    writer(msg)
+    log(msg)
   })
-  writer('----\n')
+  if (delimiter && delimiterChar) log(delimiterChar)
+}
+
+module.exports = {
+  setVerbose,
+  setDelimiter,
+  write,
 }
